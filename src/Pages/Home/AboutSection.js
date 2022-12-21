008d538e1faac669b0d96e53e8316c80492074ea
Alok <alok@Aloks-MacBook-Air.local>
2022-12-21 18:08:45 +0600

    about section added

---
+++ src/Pages/Home/AboutSection.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FaAngleRight } from 'react-icons/fa';
+
+const AboutSection = () => {
+    return (
+        <div className='text-center my-10 w-10/12 mx-auto'>
+            <h2 className='text-5xl font-bold leading-tight'>Let the journey of organizing <br /> your own learning begin</h2>
+            <p className='text-xl mt-3'>Go to the section of your choice to learn anything on any subject</p>
+            <div className='grid grid-cols-2 gap-5 mt-6'>
+                <div className="card card-side bg-base-100 border p-5">
+                    <div className='p-6'>
+                        <img src="https://i.ibb.co/ngncNpg/1200px-Backpack-icon-svg.png" className='w-20' alt="Movie" />
+                    </div>
+                    <div className="card-body text-start relative text-slate-500">
+                        <h2 className="text-2xl font-semibold">It Related Classes</h2>
+                        <p>Free live class, Free videos</p>
+                        <button className='absolute right-3 top-14'><FaAngleRight /></button>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border p-5">
+                    <div className='p-6'>
+                        <img src="https://i.ibb.co/mvtSJ0V/png-transparent-graphic-design-icon-rocket-rocket-launch-spacecraft-icon-design-symbol-retrorocket-f.png" className='w-20' alt="Movie" />
+                    </div>
+                    <div className="card-body text-start relative text-slate-500">
+                        <h2 className="text-2xl font-semibold">Skills</h2>
+                        <p>English, Freelanchin,Skills and IIT</p>
+                        <button className='absolute right-3 top-14'><FaAngleRight /></button>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border p-5">
+                    <div className='p-6'>
+                        <img src="https://i.ibb.co/9YkxLz8/332-3320668-graduation-cap-icon-png.jpg" className='w-20 h-20' alt="Movie" />
+                    </div>
+                    <div className="card-body text-start relative text-slate-500">
+                        <h2 className="text-2xl font-semibold">Addmission Test</h2>
+                        <p>University, Medical</p>
+                        <button className='absolute right-3 top-14'><FaAngleRight /></button>
+                    </div>
+                </div>
+                <div className="card card-side bg-base-100 border p-5">
+                    <div className='p-6'>
+                        <img src="https://i.ibb.co/hgMvn8Q/aaaaaaa.png" className='w-20' alt="Movie" />
+                    </div>
+                    <div className="card-body text-start relative text-slate-500">
+                        <h2 className="text-2xl font-semibold">Job Preparation</h2>
+                        <p>BCS,Bank Jobs and Govt jobs</p>
+                        <button className='absolute right-3 top-14'><FaAngleRight /></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutSection;
---
+++ src/Pages/Home/HomeBanner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight } from 'react-icons/fa';
+
+const HomeBanner = () => {
+    return (
+        <div className="bg-[url('https://5thforcesupport.com/wp-content/uploads/2022/01/GettyImages-643950510-scaled.jpg')] grid place-content-center">
+            <div className='text-center p-5 m-10'>
+                <h2 className='lg:text-6xl font-bold text-blue-500'>Edule</h2>
+                <h3 className='text-4xl my-5 font-semibold'>E-Learning Website</h3>
+                <p className='text-2xl my-5'>Let the journey of organizing your own learning begin</p>
+                <p>Go to the Courses of your choice to study any topic of any subject</p>
+                <Link to='/courses' className="btn btn-info hover:bg-blue-400 hover:text-white mt-5">Courses <FaArrowRight /> </Link>
+            </div>
+        </div>
+    );
+};
+
+export default HomeBanner;
